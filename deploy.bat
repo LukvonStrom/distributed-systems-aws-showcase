b2d31f9ffc6b7ca7b410130e2ce7696596a01b17
lfruntke <lfruntke@spirit21.com>
2020-03-01 16:24:34 +0100

    Optimize deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,8 @@
-aws cloudformation package --template-file distributed-systems-aws-showcase.yml --s3-bucket aws-ds-demo --profile eigen --output-template-file output.yml
-aws cloudformation deploy --template-file output.yml --stack-name distributed-systems-aws-showcase
+@echo off 
+
+echo "Packaging..."
+
+call aws cloudformation package --template-file distributed-systems-aws-showcase.yml --s3-bucket aws-ds-demo --output-template-file output.yml
+
+echo "Deploying..."
+call aws cloudformation deploy --template-file output.yml --stack-name distributed-systems-aws-showcase-dh --capabilities CAPABILITY_IAM
