573aba2b99e3d6c5d38d0c5781612854b464621e
lfruntke <lfruntke@spirit21.com>
2020-03-01 16:25:24 +0100

    Change SQS logic, remove hardcoded values

--- search-updater/index.js
+++ search-updater/index.js
@@ -1,5 +1,4 @@
 const { Client } = require('@elastic/elasticsearch');
-process.env.ELASTICSEARCH_DOMAIN = "https://search-es-domain-test-osi5owgcewhpekvlikeygsu23m.eu-central-1.es.amazonaws.com/"
 const client = new Client({ node: process.env.ELASTICSEARCH_DOMAIN, apiVersion: '5.6' });
 const index = "test";
 
@@ -12,10 +11,10 @@ function clean(obj) {
     return obj;
 }
 
-function cleaner(input){
+function cleaner(input) {
     let filtered_image = {}
 
-    for (let key in input){
+    for (let key in input) {
         let cleanedDataType = clean(input[key])
         filtered_image[key] = Object.values(cleanedDataType)[0]
     }
@@ -24,48 +23,47 @@ function cleaner(input){
 }
 
 
-async function exists(id){
+async function exists(id) {
     const { body } = await client.exists({
-        index: 'game-of-thrones',
+        index,
         id
     })
 }
 
-async function handle(event, context){
-    let filtered_image = cleaner(event['newImage']);
-    let filtered_keys = cleaner(event['keys']);
-    let filtered_old_image = cleaner(event['oldImage']);
+export async function handle(event, context) {
 
-    // Hat Keys im neuen "Object"
-    if(Object.keys(filtered_image).length > 0){
-        // Neuerstellung
-        if(Object.keys(filtered_old_image).length < 1 || !await exists(filtered_keys['id'])){
-            console.log(filtered_image);
+    for (const { body } in event.Records) {
+        let filtered_image = cleaner(body['newImage']);
+        let filtered_keys = cleaner(body['keys']);
+        let filtered_old_image = cleaner(body['oldImage']);
 
-            await client.index({
-                index,
-                type: 'doc',
-                body: filtered_image
-            });
+        // Has keys in new image
+        if (Object.keys(filtered_image).length > 0) {
+            // creation
+            if (Object.keys(filtered_old_image).length < 1 || !await exists(filtered_keys['id'])) {
+                console.log(filtered_image);
+
+                await client.index({
+                    index,
+                    type: 'doc',
+                    body: filtered_image
+                });
 
 
-            await client.indices.refresh({ index })
-        }else{
-            // Update
-            await client.update({
-                index,
-                type: 'doc',
-                id: filtered_keys['id'],
-                body: filtered_image
-            })
+                await client.indices.refresh({ index })
+            } else {
+                // update
+                await client.update({
+                    index,
+                    type: 'doc',
+                    id: filtered_keys['id'],
+                    body: filtered_image
+                })
 
-            await client.indices.refresh({ index })
+                await client.indices.refresh({ index })
+            }
         }
-
-
-    // löschen
     }
 
-}
 
-handle({"approximateCreationDateTime":1582119031000,"keys":{"id":{"s":"3","n":null,"b":null,"m":null,"l":null,"null":null,"bool":null,"ss":null,"ns":null,"bs":null}},"newImage":{"id":{"s":"3"}, "test": {"s": "hi"}},"oldImage":{"id":{"s":"3","n":null,"b":null,"m":null,"l":null,"null":null,"bool":null,"ss":null,"ns":null,"bs":null}},"sequenceNumber":"300000000001059532482","sizeBytes":6,"streamViewType":"NEW_AND_OLD_IMAGES"}, null).catch(console.error)
+}
