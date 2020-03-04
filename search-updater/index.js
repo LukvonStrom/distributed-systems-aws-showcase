54c226d3a034f50159fb957992a3b00db862f608
lfruntke <lfruntke@spirit21.com>
2020-03-04 18:11:25 +0100

    Improve ES Security, sign ES requests for IAM

--- search-updater/index.js
+++ search-updater/index.js
@@ -1,6 +1,20 @@
 const { Client } = require('@elastic/elasticsearch');
-const client = new Client({ node: process.env.ELASTICSEARCH_DOMAIN, apiVersion: '5.6' });
+const { createAWSConnection, awsCredsifyAll, awsGetCredentials } = require('@acuris/aws-es-connection')
+let client;
 const index = "test";
+
+async function getClient(){
+    const awsCredentials = await awsGetCredentials()
+    const AWSConnection = createAWSConnection(awsCredentials)
+    const localClient = awsCredsifyAll(
+        new Client({
+            node: process.env.ELASTICSEARCH_DOMAIN, 
+            apiVersion: '5.6',
+            Connection: AWSConnection
+        })
+    )
+    return localClient;
+}
 
 function clean(obj) {
     for (let propName in obj) {
@@ -13,11 +27,14 @@ function clean(obj) {
 
 function cleaner(input) {
     let filtered_image = {}
+    
+
 
     for (let key in input) {
         let cleanedDataType = clean(input[key])
         filtered_image[key] = Object.values(cleanedDataType)[0]
     }
+    
 
     return filtered_image;
 }
@@ -28,11 +45,14 @@ async function exists(id) {
         index,
         id
     })
+    return body;
 }
 
-export async function handle(event, context) {
-
-    for (const { body } in event.Records) {
+async function handle(event, context) {
+    client = await getClient();
+    const items = event.Records;
+    for (const l of items) {
+        let body = JSON.parse(l.body);
         let filtered_image = cleaner(body['newImage']);
         let filtered_keys = cleaner(body['keys']);
         let filtered_old_image = cleaner(body['oldImage']);
@@ -43,7 +63,7 @@ export async function handle(event, context) {
             if (Object.keys(filtered_old_image).length < 1 || !await exists(filtered_keys['id'])) {
                 console.log(filtered_image);
 
-                await client.index({
+                let result = await client.index({
                     index,
                     type: 'doc',
                     body: filtered_image
@@ -62,8 +82,12 @@ export async function handle(event, context) {
 
                 await client.indices.refresh({ index })
             }
+            
         }
     }
+    return "Success!"
 
 
 }
+
+module.exports = {handle}
